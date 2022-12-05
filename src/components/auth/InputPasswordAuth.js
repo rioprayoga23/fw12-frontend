5165006fcc0a3db182826b6976f593817903b94d
Rio Prayoga <yoprayogap@gmail.com>
2022-12-05 23:01:04 +0700

    showPassword

--- src/components/auth/InputPasswordAuth.js
+++ src/components/auth/InputPasswordAuth.js
@@ -1,17 +1,40 @@
-import React from "react";
-import { Eye } from "react-feather";
+import React, { useState } from "react";
+import { Eye, EyeOff } from "react-feather";
 
 const InputPasswordAuth = (props) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [typeInput, setTypeInput] = useState("password");
+
+  const handlerShowPassword = () => {
+    if (showPassword === false) {
+      setShowPassword(true);
+      setTypeInput("text");
+    } else {
+      setShowPassword(false);
+      setTypeInput("password");
+    }
+  };
+
   return (
     <div className="flex relative">
       <input
-        type={props.id}
+        type={typeInput}
         id={props.id}
         name={props.id}
         placeholder={props.placeholder}
         className="border-2 p-4 rounded-2xl mb-5 w-full"
       />
-      <Eye className="absolute right-4 top-5" />
+      {showPassword ? (
+        <Eye
+          className="absolute right-4 top-5 cursor-pointer"
+          onClick={handlerShowPassword}
+        />
+      ) : (
+        <EyeOff
+          className="absolute right-4 top-5 cursor-pointer"
+          onClick={handlerShowPassword}
+        />
+      )}
     </div>
   );
 };
