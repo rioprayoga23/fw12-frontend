5560e8692b4daa9e5bfcf680e001098c056de8b0
Rio Prayoga <yoprayogap@gmail.com>
2022-12-07 09:30:05 +0700

    -

--- src/components/now-showing/CardMovie.js
+++ src/components/now-showing/CardMovie.js
@@ -7,33 +7,31 @@ const CardMovie = (props) => {
     <Fragment>
       {data?.results?.map((movie) => {
         return (
-          <Fragment>
-            <div
-              className="group flex-shrink-0 h-fit p-6 mr-5 border-2 border-white rounded-lg hover:bg-white hover:border-secondary hover:shadow-md"
-              key={String(movie.id)}
-            >
-              <img
-                src={`http://localhost:8888/uploads/${movie.picture}`}
-                alt=""
-                className="w-imgMovie h-imgMovie"
-              />
-              <div className="hidden group-hover:block">
-                <div className="w-imgMovie text-center">
-                  <p className="text-[#14142B] font-bold text-lg leading-tight mt-5 font-Mulish">
-                    {movie.title}
-                  </p>
-                  <p className="text-sm text-[#A0A3BD] mt-1 font-OpenSans">
-                    {movie.genre}
-                  </p>
-                </div>
-                <Link to="/movie-details">
-                  <div className="w-full border-2 border-primary rounded-md text-center py-1 mt-5 font-Mulish text-primary text-sm cursor-pointer">
-                    Details
-                  </div>
-                </Link>
+          <div
+            className="group flex-shrink-0 h-fit p-6 mr-5 border-2 border-white rounded-lg hover:bg-white hover:border-secondary hover:shadow-md"
+            key={String(movie.id)}
+          >
+            <img
+              src={`http://localhost:8888/uploads/${movie.picture}`}
+              alt=""
+              className="w-imgMovie h-imgMovie"
+            />
+            <div className="hidden group-hover:block">
+              <div className="w-imgMovie text-center">
+                <p className="text-[#14142B] font-bold text-lg leading-tight mt-5 font-Mulish">
+                  {movie.title}
+                </p>
+                <p className="text-sm text-[#A0A3BD] mt-1 font-OpenSans">
+                  {movie.genre}
+                </p>
               </div>
+              <Link to="/movie-details">
+                <div className="w-full border-2 border-primary rounded-md text-center py-1 mt-5 font-Mulish text-primary text-sm cursor-pointer">
+                  Details
+                </div>
+              </Link>
             </div>
-          </Fragment>
+          </div>
         );
       })}
     </Fragment>
